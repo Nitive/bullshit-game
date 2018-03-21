bd55c82891a1f3ba7935cde2085398fa19c4f444
Maxim Samoilov <nitive@icloud.com>
2018-03-21 08:48:35 +0300

    Add ability to redefine dev server PORT

--- modules/build/dev-server.ts
+++ modules/build/dev-server.ts
@@ -31,7 +31,7 @@ app.get('/*', (req, res) => {
     })
 })
 
-app.listen(5000, (err?: Error) => {
+app.listen(process.env.PORT || 5000, (err?: Error) => {
   /* eslint-disable no-console */
   if (err) {
     console.error(err)

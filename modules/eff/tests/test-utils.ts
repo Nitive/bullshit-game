cbd4da8058bafd93b0ea4b772132ef7325d777c5
Maxim Samoilov <nitive@icloud.com>
2018-04-08 22:29:26 +0300

    simplify areStreamsEqual test util by using jest array comparing

--- modules/eff/tests/test-utils.ts
+++ modules/eff/tests/test-utils.ts
@@ -21,17 +21,9 @@ export function getStreamValues<T>(stream: Stream<T>): Promise<T[]> {
   )
 }
 
-export function areStreamsEqual<T>(a: Stream<T>, b: Stream<T>, equal = (a: T, b: T) => a === b): Promise<void> {
+export function areStreamsEqual<T>(a: Stream<T>, b: Stream<T>): Promise<void> {
   return Promise.all([getStreamValues(a), getStreamValues(b)])
     .then(([aValues, bValues]) => {
-      if (aValues.length !== bValues.length) {
-        throw new Error(`${a} and ${b} has different length`)
-      }
-
-      for (let i = 0; i < aValues.length; i += 1) {
-        if (!equal(aValues[i], bValues[i])) {
-          throw new Error(`${i} element is different: ${aValues[i]} is not equal ${bValues[i]}`)
-        }
-      }
+      expect(aValues).toEqual(bValues)
     })
 }

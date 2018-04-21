b21ce40fc92dd3a2e194d4bb70d381a590117f76
Maxim Samoilov <nitive@icloud.com>
2018-04-21 12:36:36 +0300

    Add router-driver

---
+++ modules/eff/router-driver/index.ts
@@ -0,0 +1,89 @@
+import xs, { Stream } from 'xstream'
+import { Driver, Effect, EffectsDescriptor } from '../core/run'
+import { selectEffectByType } from '../core/select'
+import { History, Location } from 'history'
+
+export type RouterAction
+  = { type: 'push', location: string }
+  | { type: 'replace', location: string }
+
+export interface RouterEffect extends Effect {
+  effectType: 'router',
+  sink$: Stream<RouterAction>
+}
+
+export function routerEff(action$: Stream<RouterAction>): RouterEffect {
+  return {
+    effectType: 'router',
+    sink$: action$,
+  }
+}
+
+export function selectRouterEff<Action>(vnode: EffectsDescriptor): Stream<Action> {
+  return selectEffectByType<Action>('router', vnode, xs.merge)
+}
+
+function handleRouterAction(history: History) {
+  return (action: RouterAction) => {
+    switch (action.type) {
+      case 'push':
+        history.push(action.location)
+        break
+
+      case 'replace':
+        history.replace(action.location)
+        break
+    }
+  }
+}
+
+export class RouterSource {
+  location$: Stream<Location>
+
+  constructor(history: History, action$: Stream<RouterAction>) {
+    this.location$ = getLocation$(history).endWhen(action$.last())
+    action$.addListener({
+      next: handleRouterAction(history),
+      error(err) {
+        throw new Error(err)
+      },
+    })
+  }
+
+  push(location: string): RouterAction {
+    return { type: 'push', location }
+  }
+
+  replace(location: string): RouterAction {
+    return { type: 'replace', location }
+  }
+}
+
+function getLocation$(history: History): Stream<Location> {
+  let unlisten: () => void
+
+  return xs
+    .create<Location>({
+      start(listener) {
+        unlisten = history.listen(location => {
+          listener.next(location)
+        })
+      },
+      stop() {
+        unlisten()
+      },
+    })
+    .startWith(history.location)
+}
+
+
+export function makeRouterDriver(history: History): Driver<Stream<RouterAction>, RouterSource> {
+  return {
+    run(action$) {
+      return new RouterSource(history, action$)
+    },
+    select(effects): Stream<RouterAction> {
+      return selectRouterEff(effects)
+    },
+  }
+}
---
+++ modules/eff/tests/router.test.ts
@@ -0,0 +1,38 @@
+import { VNode } from 'snabbdom/vnode'
+import xs, { Stream } from 'xstream'
+import fromDiagram from 'xstream/extra/fromDiagram'
+import { EffectsDescriptor, run } from '../core/run'
+import { RouterSource, RouterAction, routerEff, makeRouterDriver } from '../router-driver'
+import { areStreamsEqual, makeMultistepDone } from './test-utils'
+import { createMemoryHistory } from 'history'
+
+describe('router', () => {
+  it('router eff should works', done => {
+    interface Sources {
+      router: RouterSource
+    }
+
+    interface Sinks {
+      router: Stream<RouterAction>
+    }
+
+    function app(sources: Sources): EffectsDescriptor {
+      const expected = fromDiagram('0-1-3|', { values: { 0: '/', 1: '/test', 3: '/test2' } })
+      const pathname$ = sources.router.location$.map(x => x.pathname)
+      areStreamsEqual(pathname$, expected).then(done).catch(done)
+
+      return (
+        routerEff(xs.from<RouterAction>([
+          { type: 'push', location: '/test' },
+          { type: 'push', location: '/test2' },
+        ]))
+      )
+    }
+
+    const history = createMemoryHistory()
+
+    run<Sources, Sinks>(app, {
+      router: makeRouterDriver(history),
+    })
+  })
+})
